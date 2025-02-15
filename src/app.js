eee3e72eef63001f7200a859a3c8d3f95fad106a
SamToca <swtocaimaza@gmail.com>
2025-02-15 14:46:58 -0300

    cors ahora siii

--- src/app.js
+++ src/app.js
@@ -16,7 +16,10 @@ const connection = mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: 'https://front-adoptame.vercel.app',
+    credentials: true
+}));
 
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
